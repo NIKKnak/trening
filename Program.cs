4ba20286c258006f57c4d98040c4c07fefd59b1c
NIKKnak <nik88350759@yandex.ru>
2023-10-30 12:53:46 +0300

    test

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+using System.Net;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = @"C:\Users\nik88\OneDrive\Desktop\Текстовый документ.txt";
+
+        string text = "sdfdsfdsf";
+
+
+        using (StreamWriter stream = new StreamWriter(path, true))
+        {
+            stream.WriteLine(text);
+        }
+
+
+
+
+
+
+    }
+
+
+}
